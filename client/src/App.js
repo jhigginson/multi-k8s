3e18dd918c5bb6a8d35af5e55d7c8279ed4e3cbd
John Higginson <jhigginson@gmail.com>
2022-07-15 14:11:49 -0700

    configure for k8s

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <div className="bg-slate-800 text-xl flex flex-col h-full">
         <header className="bg-sky-700 text-3xl font-['Permanent_Marker'] text-sky-100 p-4 flex justify-between">
-          <h1 className="text-pink-400">Fib</h1>
+          <h1 className="text-pink-400">Fib Calculator V2</h1>
           <div className="flex justify-between w-fit">
             <Link className="px-3" to="/">Home</Link>
             <Link className="px-3" to="/otherpage">Other Page</Link>
